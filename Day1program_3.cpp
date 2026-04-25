fbc8318971a43fc65a90004d857fc7e3bdc57a3c
akshit <108526799+fnsjkd@users.noreply.github.com>
2026-04-25 17:30:22 +0530

    My 2nd Program

---
+++ Day1program_3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 9315958828;
+    
+    cout << a << endl;
+    
+    char b = 'p';
+    
+    cout<< b <<endl;
+    
+    bool bl = false;
+    cout<< bl << endl;
+    
+    double d = 1.98;
+    cout<< d << endl;
+    
+    float f = 1.435;
+    cout<< f << endl;
+    
+    int size = sizeof(b);
+    cout<< "size of a is:" <<size <<endl;
