2dfa7dc29aae89e088c14fe45df7ec130aecedec
akshit <108526799+Rizzy-prgm@users.noreply.github.com>
2026-04-26 18:32:58 +0530

    Day 2 program 1

---
+++ Day2Program_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   //int a;
+    //cin>>a;
+    
+    //cout<<"volue of n is :" << n <<endl;
+    
+    //if a is positive
+    /*
+    if(a>0) {
+        cout<<"A is positive" <<endl;
+    }
+    else{
+        cout<<"A is negative" <<endl;
+    }
+    */
+    
+    int a,b;
+    
+    //cin>> a >> b;
+    
+    a = cin.get();
+    
+//cout<<" value of a and b is "<<a <<" "<< b <<endl;
+
+ cout<<"value of a is : "<< a << endl;
+    
+    
+}
