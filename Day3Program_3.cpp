38ea6454696a723bc994833987aa7861dbf88ecc
akshit <108526799+Rizzy-prgm@users.noreply.github.com>
2026-04-27 16:09:41 +0530

    Day3Program_3

---
+++ Day3Program_3.cpp
@@ -0,0 +1,16 @@
+class solution {
+public:
+    int bitwiseComplement(int n) {
+        
+        int m = n;
+        int mask = 0;
+        
+        while( n!=0) {
+         mask = (mask << 1) | 1;
+         m = m >> 1;
+    }
+    int ans = (-n) & mask;
+    
+        return ans;
+    }
+};
